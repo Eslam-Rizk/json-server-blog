25b334f62e488088b7bbebfadbeb3ecef4ecbd9e
mosaleh <mosaleh@mail.com>
2025-06-16 00:40:23 +0300

    add cors

--- server-api.cjs
+++ server-api.cjs
@@ -1,18 +1,35 @@
-// serve-api.cjs
-const jsonServerAuth = require("json-server-auth");
 const jsonServer = require("json-server");
-
+const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-// Bind the middlewares and auth
-server.use(middlewares);
+// Bind the router db to the app
 server.db = router.db;
-server.use(jsonServerAuth);
+
+server.use(middlewares);
+
+// Enable CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS",
+  );
+  next();
+});
+
+// Auth middleware (should be placed before the router)
+server.use(auth);
+
+// Main router
 server.use(router);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`JSON Server Auth is running on port ${PORT}`);
+  console.log(`JSON Server is running with auth on port ${PORT}`);
 });
